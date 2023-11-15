a68be97d479084fb597db7a3ada435191f76d452
rafael-dk <rafael.daiki@unesp.br>
2023-11-15 18:22:17 -0300

    Update fun_2_e_3.c

--- fun_2_e_3.c
+++ fun_2_e_3.c
@@ -1,29 +1,35 @@
 #include "header.h"
 
+//armazenamento chave-endereco usado no indice
 typedef struct indice {
     char string[19];
     size_t endereco;
 } INDICE;
 
+//bloco considerado o "no" da arvore b
 typedef struct pagina {
     short qtd_dados;
     INDICE dado[3];
     short ponteiro[4];
 } PAGINA;
 
+//struct para manipular o arquivo e a raiz da arvore b a ser utilizada
 typedef struct arv_b {
     short raiz;
     FILE* arq;
 } ARV_B;
 
+//converte o rrn(ponteiro) em endereco no arquivo de indices
 size_t rrn_converter(short ponteiro) {
     return sizeof(size_t)+sizeof(PAGINA)*ponteiro;
 }
 
+//realiza a leitura da pagina previamente posicionada com fseek
 void extrair_pagina(PAGINA* pag, ARV_B* arvore) {
     fread(pag, sizeof(PAGINA), 1, arvore->arq);
 }
 
+//impressao do registro do arquido de dados original com base no endereco de memoria
 void imprimir_registro(size_t endereco, FILE* arquivoDados) {
 
     fseek(arquivoDados, endereco, SEEK_SET);
@@ -61,17 +67,23 @@ void imprimir_registro(size_t endereco, FILE* arquivoDados) {
     free(registro);
 }
 
+//funcao usada para imprimir os registros de uma dada arvore b
 void listar_pagina(FILE* arq_dados, ARV_B* arvore) {
+
+    //extracao da pagina previamente posicionada
     PAGINA pag;
     extrair_pagina(&pag, arvore);
 
+    //checagem caso os rrn (ponteiros) existam, procurando os menores itens da arvore
     if (pag.ponteiro[0] !=-1) {
         fseek(arvore->arq, rrn_converter(pag.ponteiro[0]), SEEK_SET);
         listar_pagina(arq_dados, arvore);
     }
 
+    //impressao do primeiro item apos as recursoes
     imprimir_registro(pag.dado[0].endereco, arq_dados);
 
+    //logica similar as anteriores
     if (pag.ponteiro[1] !=-1) {
         fseek(arvore->arq, rrn_converter(pag.ponteiro[1]), SEEK_SET);
         listar_pagina(arq_dados, arvore);
@@ -94,6 +106,7 @@ void listar_pagina(FILE* arq_dados, ARV_B* arvore) {
     }
 }
 
+//lista todos os dados se houver dados na arvore b
 void listar_dados(FILE* arq_dados, ARV_B* arvore) {
     if (arvore->raiz != -1) {
         fseek(arvore->arq, rrn_converter(arvore->raiz), SEEK_SET);
@@ -101,24 +114,30 @@ void listar_dados(FILE* arq_dados, ARV_B* arvore) {
     }
 }
 
+//funcao para usada para pesquisa de uma chave na arvore b, retorna a posicao da chave e, caso inexistente, -1.
 short pesquisar_dado(STRING* chave, short pagina, ARV_B* arvore) {
     PAGINA pag;
     extrair_pagina(&pag, arvore);
     
+    //compara a chave com o primeiro valor de indices
+    //se for menor, checa o rrn anterior a ela
     if(strcpm(*chave, pag.dado[0].string) == -1)  {
         if (pag.ponteiro[0] != -1) {
             fseek(arvore->arq, rrn_converter(pag.ponteiro[0]), SEEK_SET);
             return pesquisar_dado(chave, pag.ponteiro[0], arvore);
         } else {
-            printf("\nChave nao encontrada: %s.\n", *chave);
+            printf("\nChave nao encontrada: %s.\n", *chave); //imprimi caso seja = -1 (chave nao existe)
             return -1;
         }
     }
 
+    //se for igual, printa que encontrou e retorna uma compilacao de sua posicao
     if(strcpm(*chave, pag.dado[0].string) == 0) {
-        printf("\nChave encontrada: %s. (Pag: %d | Pos: %d)\n", *chave, pagina, 0);
+        printf("\nChave encontrada: %s. (Pag: %d | Pos: %d)\n", *chave, pagina, 0); //imprimi em caso de exista
         return pagina*10;
     }
+
+    //se for maior compara com o proximo (se ele existir)
 
     if(strcpm(*chave, pag.dado[1].string) == -1)  {
         if (pag.ponteiro[1] != -1) {
@@ -130,7 +149,7 @@ short pesquisar_dado(STRING* chave, short pagina, ARV_B* arvore) {
         }
     }
 
-    if(strcpm(*chave, pag.dado[1].string) == 0) {
+    if(strcpm(*chave, pag.dado[1].string) == 0 && pag.qtd_dados > 1) {
         printf("\nChave encontrada: %s. (Pag: %d | Pos: %d)\n", *chave, pagina, 1);
         return pagina*10 + 1;
     }
